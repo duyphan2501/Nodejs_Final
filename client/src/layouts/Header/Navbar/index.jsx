2de87b705e7690c115be867894fc14186f6e2323
DucHana <ducx345@gmail.com>
2025-11-12 16:45:41 +0700

    add search

--- client/src/layouts/Header/Navbar/index.jsx
+++ client/src/layouts/Header/Navbar/index.jsx
@@ -6,6 +6,7 @@ import CartIcon from "./CartIcon";
 import { MyContext } from "../../../Context/MyContext";
 import useUserStore from "../../../store/useUserStore";
 import { useNavigate } from "react-router-dom";
+import Search from "./Search";
 
 const Navbar = ({
   menuData,
@@ -137,22 +138,7 @@ const Navbar = ({
         {/* Right Icons */}
         <div className="flex items-center space-x-2 md:space-x-3 ">
           {/* Search */}
-          <button className="p-2 hover:bg-gray-100 rounded cursor-pointer">
-            <svg
-              className="w-5 h-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
+          <Search />
 
           {/* Account Menu Button */}
           {!user ? (
